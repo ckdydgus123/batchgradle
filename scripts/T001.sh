4d4a7ef507ebc8e8b26456e6cb1230e82da6a044
Ui2Ivan <45223464+Ui2Ivan@users.noreply.github.com>
2019-10-17 19:07:09 +0900

    setup HEROKU

--- scripts/T001.sh
+++ scripts/T001.sh
@@ -5,6 +5,6 @@ cd ../
 
 source ./scripts/env/classpath.sh
 
-java -cp "${CLASSPATH}" org.springframework.batch.core.launch.support.CommandLineJobRunner META-INF/jobs/T001.xml T001
+java -cp "${CLASSPATH}" org.springframework.batch.core.launch.support.CommandLineJobRunner src/main/resource/META-INF/jobs/T001.xml T001
 exit $?
 
